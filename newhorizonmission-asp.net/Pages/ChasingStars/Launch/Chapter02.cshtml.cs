2b56dac53f7155c853b3960f008fc513baafffa3
rwhite83 <rwhite83@hotmail.com>
2020-04-05 16:41:28 -0700

    finished most of port

---
+++ newhorizonmission-asp.net/Pages/ChasingStars/Launch/Chapter02.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace newhorizonmission_asp.net.Pages.ChasingStars.Launch
+{
+    public class Chapter02Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ newhorizonmission-asp.net/Pages/Dispatches/Updates/Update_20200312.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace newhorizonmission_asp.net.Pages.Dispatches.Updates
+{
+    public class Update_20200312Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
