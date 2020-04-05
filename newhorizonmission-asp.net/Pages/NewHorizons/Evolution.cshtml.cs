f5afafa0823ef4ecdb1f096d8d1ada99e3cb7029
rwhite83 <rwhite83@hotmail.com>
2020-04-05 00:58:38 -0700

    reconstructed new horizon section

---
+++ newhorizonmission-asp.net/Pages/NewHorizons/Evolution.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace newhorizonmission_asp.net.Pages.NewHorizons
+{
+    public class EvolutionModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ newhorizonmission-asp.net/Pages/NewHorizons/Reunion/Chapter01.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace newhorizonmission_asp.net.Pages.NewHorizons.Reunion
+{
+    public class Chapter01Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
